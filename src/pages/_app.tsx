04018a21f9c1624d44f3f3fee3a8d702d0713732
Ludal <ludovic.chombeau@gmail.com>
2023-04-27 23:06:38 +0200

    chore: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { FC } from "react";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <ChakraProvider>
     <Component {...pageProps} />
+    <Analytics />
   </ChakraProvider>
 );
 
