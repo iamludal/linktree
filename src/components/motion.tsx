66accbe92bbfc730b7e1f8665f279878008d110e
Ludal <contact@iamludal.fr>
2024-09-22 09:32:00 +0200

    fix: motion props

--- src/components/motion.tsx
+++ src/components/motion.tsx
@@ -1,5 +1,14 @@
 import { Link, LinkProps, Stack, StackProps } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { ForwardedRef, forwardRef } from "react";
 
-export const MotionLink = motion<Omit<LinkProps, "transition">>(Link);
-export const MotionStack = motion<Omit<StackProps, "transition">>(Stack);
+const ChakraLink = (props: LinkProps, ref: ForwardedRef<HTMLAnchorElement>) => {
+  return <Link {...props} ref={ref} />;
+};
+
+const ChakraStack = (props: StackProps, ref: ForwardedRef<HTMLDivElement>) => {
+  return <Stack {...props} ref={ref} />;
+};
+
+export const MotionLink = motion(forwardRef(ChakraLink));
+export const MotionStack = motion(forwardRef(ChakraStack));
