a1a87e97b22d5d957d477e3b675a1244073f4b42
Ludal <contact@iamludal.fr>
2025-09-26 23:39:51 +0200

    feat(vercel): add speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { FC } from "react";
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <ChakraProvider>
     <Component {...pageProps} />
     <Analytics />
+    <SpeedInsights />
   </ChakraProvider>
 );
 
