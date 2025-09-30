b52a83330d183d2532e15172637afb32998ab722
Ludal <44783088+iamludal@users.noreply.github.com>
2025-09-30 10:27:03 +0200

    chore(nextjs): redirect not-found routes to root

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+        permanent: true,
+      }
+    ]
+  }
 };
 
 export default nextConfig;
